328ecfc39b5125682744fd9fe17ea438aecdb725
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-04-18 09:10:11 +0530

    added login and create account buttons inside header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,20 +2,49 @@ import React, { Component } from "react";
 import './Header.css';
 import logo from '../../assets/logo.png';
 import { Wave } from "react-animated-text";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/styles";
+
+const styles = theme => ({
+   loginButton: {
+       textTransform: 'none',
+       color: 'white',
+       marginRight: '1rem',
+       '&:hover': {
+           backgroundColor: 'transparent !important',
+           color: 'black',
+       }
+   },
+    signUpButton: {
+       textTransform: 'none',
+        color: 'white',
+        marginRight: '1rem',
+        borderColor: 'white',
+        '&:hover': {
+           borderColor: 'black',
+            color: 'black',
+        }
+    }
+});
 
 class Header extends Component {
     render() {
+        const { classes } = this.props;
         return (
             <div className="header-div">
                 <div className="app-logo-image">
                     <img src={logo} alt={""}/>
                 </div>
-                <div className={"app-logo-text"}>
+                <div className="app-logo-text">
                     <Wave text={"Food On Way"} />
+                </div>
+                <div className="header-buttons">
+                    <Button className={classes.loginButton} disableRipple={true} >Log in</Button>
+                    <Button className={classes.signUpButton} disableRipple={true} variant={"outlined"} >Create an account</Button>
                 </div>
             </div>
         );
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
